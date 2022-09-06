b5def102f10f5755a19fdc6ca1f8840f5c55e6ab
DiegoV <ddvallejoj@gmail.com>
2022-09-06 13:11:37 -0600

    seriegeometrica.cpp

---
+++ SO/new 1.cpp
@@ -0,0 +1,90 @@
+/*
+ * UNIVERSIDAD DEL ISTMO
+ * Sistemas Operativos
+ * Date:	2022/08/25
+ * Ver:		2.0 - 2022/08/19
+ *
+ * busywaitPI.cpp
+ * Calculo de PI usando busy-waiting para proteger memoria compartida
+ */
+ 
+#include <iostream>
+#include <stdio.h>		//printf
+#include <cmath>		//pow
+#include <pthread.h>	
+
+using namespace std;
+
+const long long N = pow(10,10);
+//---- Cuidar que THREAD_COUNT sea factor de N
+const int THREAD_COUNT = 10;
+
+//---- suma y bandera globales
+long double sum = 0.0;
+int flag = 0;
+
+//------------
+// Serie para calculo de PI
+//------------
+void *threadSum(void *thread_rank)
+{
+	long my_rank = (long)thread_rank;
+    double, my_sum = 0.0;  //*** Suma local
+	long long i;
+	//---- Lote de datos de cada thread
+	long long my_n = N/THREAD_COUNT;
+	//---- Rango de datos para cada hilo
+	long long my_first_i = my_n * my_rank;
+	long long my_last_i = my_first_i + my_n;
+	
+	if(my_first_i % 2 == 0)		//Orden de dato inicial es para
+		factor = 1.0;
+	else
+		factor = -1.0;
+	
+	//---- Magnitud de cada elemento de la serie
+	for(i=my_first_i; i<my_last_i; i++)
+	{
+		my_sum += 1/(i*(i+1));  //*** Suma local
+	}
+
+	//---- Busy-waiting para actualizar variable y bandera global
+	while(flag != my_rank);
+		sum += my_sum;  //Copia del resultado parcial del hilo a mem-global
+	flag = (flag + 1) % THREAD_COUNT;
+	
+	return NULL;
+}
+
+int main()
+{
+	int i, rc;
+	pthread_t threads[THREAD_COUNT];
+	
+	for(i=0;i<THREAD_COUNT;i++)
+	{
+		rc = pthread_create(&threads[i],NULL,threadSum,(void *)i);
+		if(rc)
+		{
+			cout<<">>>Error creating threads"<<endl;
+			exit(-1);
+		}
+		/*
+		rc = pthread_join(threads,NULL);
+		if(rc)
+		{
+			cout<<"No se pudo unir el hilo "<<i<<endl;
+			exit(-1);
+		}*/
+	}
+	for(i=0;i<THREAD_COUNT;i++) {
+		rc = pthread_join(threads[i],NULL);
+		if(rc)
+		{
+			cout<<"No se pudo unir el hilo "<<i<<endl;
+			exit(-1);
+		}
+	}
+	printf("PI= %.10Lf\n",4.0*sum);
+	return 0;
+}
---
+++ SO/serieGeometrica.cpp
@@ -0,0 +1,81 @@
+/*
+ * UNIVERSIDAD DEL ISTMO
+ * Sistemas Operativos
+ *
+ * Serie.cpp
+ * Diego V
+ */
+ 
+#include <iostream>
+#include <stdio.h>		//printf
+#include <cmath>		//pow
+#include <pthread.h>	
+
+using namespace std;
+
+const long long N = pow(10,10);
+//---- Cuidar que THREAD_COUNT sea factor de N
+const int THREAD_COUNT = 10;
+
+//---- suma y bandera globales
+long double sum = 0.0;
+int flag = 0;
+
+pthread_mutex_t vestidor; 
+//------------
+// Calculo de serie ejercicio
+//------------
+void *threadSum(void *thread_rank)
+{
+	long my_rank = (long)thread_rank;
+    double factor, my_sum = 0.0;  //*** Suma local
+	long long i;
+	//---- Lote de datos de cada thread
+	long long my_n = N/THREAD_COUNT;
+	//---- Rango de datos para cada hilo
+	long long my_first_i = my_n * my_rank;
+	long long my_last_i = my_first_i + my_n;
+	
+	//---- Magnitud de cada elemento de la serie
+	for(i=my_first_i; i<my_last_i; i++)
+	{
+		my_sum += 1/(i*(i+1));  //*** Suma local
+
+	}
+	pthread_mutex_lock(&vestidor);
+
+	//---- Busy-waiting para actualizar variable y bandera global
+	sum += my_sum;  //Copia del resultado parcial del hilo a mem-global
+	//flag = (flag + 1) % THREAD_COUNT;
+	pthread_mutex_unlock(&vestidor);
+	pthread_exit(NULL);
+	
+	return NULL;
+}
+
+int main()
+{
+	long long i, rc;
+	pthread_t threads[THREAD_COUNT];
+	pthread_mutex_init(&vestidor, NULL);
+	for(i=0;i<THREAD_COUNT;i++)
+	{
+		rc = pthread_create(&threads[i],NULL,threadSum,(void *)i);
+		if(rc)
+		{
+			cout<<">>>Error creating threads"<<endl;
+			exit(-1);
+		}
+	}
+	for(i=0;i<THREAD_COUNT;i++) {
+		rc = pthread_join(threads[i],NULL);
+		if(rc)
+		{
+			cout<<"No se pudo unir el hilo "<<i<<endl;
+			exit(-1);
+		}
+	}
+	pthread_mutex_destroy(&vestidor);
+	printf("PI= %.10Lf\n",4.0*sum);
+	return 0;
+}
