ad17dcc7e3eaa01f89061c1a567ff16696e8d4d6
DiegoV <ddvallejoj@gmail.com>
2022-09-07 22:46:33 -0600

    vectors

--- SO/utilidad.cpp
+++ SO/utilidad.cpp
@@ -9,18 +9,27 @@
 #include <iostream>
 #include <stdio.h>		
 #include <cmath>		
-#include <pthread.h>	
+#include <pthread.h>
+#include <vector>
 
 using namespace std;
 
 #define JULIO 8;
 long long ventas_julio = 0.0;
 long long utilidad_julio = 0.0;
+vector<int> uVentas_julio{300, 400, 1590, 200, 390, 1455, 800, 60};
 #define AGOSTO 8;
 long long ventas_agosto = 0.0;
 long long utilidad_agosto = 0.0;
+vector<int> uAgosto{250, 380, 800, 250, 600, 1200, 1540, 15};
+
 //el join de los hilos por separadoooooo
 pthread_mutex_t vestidor; 
+
+vector<double> precio{60.00, 32.00, 22.00, 24.00, 28.00, 24.00, 32.00, 60.00};
+vector<double> costo{20.00, 19.20, 13.20, 17.20, 20.10, 17.20, 23.00, 20.00};
+
+
 
 void *julio(void *item) {
     return NULL;
